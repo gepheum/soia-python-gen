9c3dd5a7dafa37ff6d156d4657547909fba7817b
Tyler Fibonacci <gepheum@gmail.com>
2024-11-02 09:18:59 -0400

    fix UTs

--- e2e-test/tests/test_soiagen.py
+++ e2e-test/tests/test_soiagen.py
@@ -1,7 +1,7 @@
 import unittest
 
 from soiagen.full_name import FullName
-from soiagen.structs import Foo, True_
+from soiagen.structs import Color, Foo, Triangle, True_
 
 
 class SoiagenTestCase(unittest.TestCase):
@@ -12,8 +12,12 @@ class SoiagenTestCase(unittest.TestCase):
 
     def test_nested_struct(self):
         bar = Foo.Bar(bar="Bar")
-        self.assertEqual(bar.foos, ())
+        self.assertEqual(bar.foos, None)
         self.assertEqual(bar.bar, "Bar")
+
+    def test_triangle_default(self):
+        self.assertEqual(Triangle.DEFAULT.color, Color.DEFAULT)
+        self.assertEqual(Triangle.DEFAULT.points, ())
 
     def test_struct_with_name_conflicts(self):
         t = True_(self=())
