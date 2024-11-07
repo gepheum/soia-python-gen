57786c7d85272d11520b9b25041c187d51d889c5
Tyler Fibonacci <gepheum@gmail.com>
2024-11-07 11:57:31 -0500

    handle KeyedItems

--- e2e-test/tests/test_soiagen.py
+++ e2e-test/tests/test_soiagen.py
@@ -6,9 +6,13 @@ from soiagen.structs import Color, Foo, Triangle, True_
 
 class SoiagenTestCase(unittest.TestCase):
     def test_full_name(self):
-        full_name = FullName(first_name="Osi", last_name="Daro")
-        self.assertEqual(full_name.first_name, "Osi")
-        self.assertEqual(full_name.last_name, "Daro")
+        full_name = FullName(first_name="Tyler", last_name="Fibonacci")
+        self.assertEqual(full_name.first_name, "Tyler")
+        self.assertEqual(full_name.last_name, "Fibonacci")
+        self.assertEqual(
+            FullName.SERIALIZER.to_json_code(full_name),
+            '["Tyler","Fibonacci"]',
+        )
 
     def test_nested_struct(self):
         bar = Foo.Bar(bar="Bar")
